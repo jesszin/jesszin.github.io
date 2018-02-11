59641231953468c1cb208110f4458da9c3cb55dc
jinjess <jinareum.76@gmail.com>
2018-02-12 05:03:43 +0900

    index

--- js/index.js
+++ js/index.js
@@ -1,7 +1,15 @@
-$(document).ready(function(){
-	$('#navicon').click(function(){
-		$(this).toggleClass('open');
-		$('.navWrap').slideToggle();
-	});
-	
-});//end
+$(window).scroll(function() { 
+    if ($(this).scrollTop()> 30) { 
+        $('#navbar').fadeIn(500); 
+    } else { 
+        $('#navbar').fadeOut(300); 
+    } 
+});
+/*navigation js*/
+$(function() {
+  $('.main-button').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 800, 'linear');
+  });
+});
+/*main-button js*/
