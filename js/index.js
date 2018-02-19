bc9cec33d9a20d3808fe584c2f246723c4c2aab0
jinjess <jinareum.76@gmail.com>
2018-02-20 00:44:45 +0900

    index edit

--- js/index.js
+++ js/index.js
@@ -7,32 +7,6 @@ $(window).scroll(function () {
 });
 /*-----navigation js end*/
 
-
-$(function () {
-    $('.main-button').on('click', function (e) {
-        e.preventDefault();
-        $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top
-        }, 800, 'linear');
-    });
-});
-
-/*-----main-button js end*/
-
-/*
-$(function () {
-    $('.navbar-collapse ul li>a').on('click', function (e) {
-        e.preventDefault();
-        $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top
-        }, 600, 'linear');
-    });
-});
-*/
-/*----nav js end*/
-
-
-
 $(function () {
 $(".main-slide > div:gt(0)").hide();
 
@@ -46,3 +20,70 @@ setInterval(function () {
 }, 4000);
     });
 /*main slide js end*/
+
+
+
+/*
+$(function () {
+    $('.main-button').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 800, 'linear');
+    });
+});
+
+//main-button js end
+
+
+$(function () {
+    $('.navbar-collapse ul li>a').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 600, 'linear');
+    });
+});
+//----nav js end
+
+*/
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+    
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+        
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+      
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+2
+        }, 800, 'linear', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+});
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('.navbar-collapse a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('.navbar-collapse ul li a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
+}
+
